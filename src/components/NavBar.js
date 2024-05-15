08ae670dd4a3467f63d56f51cba1dadfc3d3a54a
AlexVila0204 <alberth@unitec.edu>
2024-05-15 02:13:23 -0600

    trabajando en la NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import img1 from "../img/utube_icon.ico";
 import {
   Navbar,
   Container,
@@ -6,47 +7,62 @@ import {
   Form,
   Button,
   Nav,
+  Offcanvas,
 } from "react-bootstrap";
-
 export default class NavBar extends Component {
   render() {
     return (
-      <Navbar expand="lg" className="navBar">
+      <Navbar expand="xxl" className="navBar">
         <Container fluid>
-          <Navbar.Brand href="#" className="brand-text">Navbar scroll :O</Navbar.Brand>
-          <Navbar.Toggle aria-controls="navbarScroll" />
-          <Navbar.Collapse id="navbarScroll">
-            <Nav
-              className="me-auto my-2 my-lg-0"
-              style={{ maxHeight: "100px" }}
-              navbarScroll
-            >
-              <Nav.Link href="#action1">Home</Nav.Link>
-              <Nav.Link href="#action2">Link</Nav.Link>
-              <NavDropdown title="Link" id="navbarScrollingDropdown">
-                <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action4">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action5">
-                  Something else here
-                </NavDropdown.Item>
-              </NavDropdown>
-              <Nav.Link href="#" disabled>
-                Link
-              </Nav.Link>
-            </Nav>
-            <Form className="d-flex">
-              <Form.Control
-                type="search"
-                placeholder="Search"
-                className="me-2"
-                aria-label="Search"
-              />
-              <Button variant="outline-success">Search</Button>
-            </Form>
-          </Navbar.Collapse>
+        <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-xxl`}  />
+          <Navbar.Offcanvas
+            id={`offcanvasNavbar-expand-xxl`}
+            aria-labelledby={`offcanvasNavbarLabel-expand-xxl`}
+            placement="end"
+          >
+            <Offcanvas.Header closeButton>
+              <Offcanvas.Title id={`offcanvasNavbarLabel-expand-xxl`}>
+                Offcanvas
+              </Offcanvas.Title>
+            </Offcanvas.Header>
+            <Offcanvas.Body>
+              <Nav className="justify-content-end flex-grow-1 pe-3">
+                <Nav.Link href="#action1">Home</Nav.Link>
+                <Nav.Link href="#action2">Link</Nav.Link>
+                <NavDropdown
+                  title="Dropdown"
+                  id={`offcanvasNavbarDropdown-expand-xxl`}
+                >
+                  <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+                  <NavDropdown.Item href="#action4">
+                    Another action
+                  </NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action5">
+                    Something else here
+                  </NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+              <Form className="d-flex">
+                <Form.Control
+                  type="search"
+                  placeholder="Search"
+                  className="me-2"
+                  aria-label="Search"
+                />
+                <Button variant="outline-success">Search</Button>
+              </Form>
+            </Offcanvas.Body>
+          </Navbar.Offcanvas>
+          <Navbar.Brand href="#" className="me-auto">
+            <img
+              src={img1}
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+              alt="YouTube Icon"
+            />
+          </Navbar.Brand>
         </Container>
       </Navbar>
     );
